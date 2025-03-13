9889bce0bd0f1fa5fec4a34a979f3d7d1a1c38f0
Schola Janvier <90581570+jkschola@users.noreply.github.com>
2025-03-13 01:57:22 +0100

    Modify column names

--- models/marts/Int_erp__customer_locations.sql
+++ models/marts/Int_erp__customer_locations.sql
@@ -4,7 +4,7 @@ WITH erp_customer_locations AS (
         {{ ref('stg_erp__customer_locations') }}
 )
 SELECT
-    cid AS customer_key,
-    cntry AS country
+    customer_key,
+    country
 FROM 
     erp_customer_locations
