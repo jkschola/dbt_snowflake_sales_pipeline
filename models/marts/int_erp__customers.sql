50c44a102043f68c4f2a21e9604edc62b5a37e54
Schola Janvier <90581570+jkschola@users.noreply.github.com>
2025-03-13 02:30:42 +0100

    Remove testing columns and keep final column names

--- models/marts/int_erp__customers.sql
+++ models/marts/int_erp__customers.sql
@@ -4,24 +4,21 @@ WITH erp_customers_cleansed AS (
         {{ ref('stg_erp__customers') }}
 )
 SELECT
-    customer_key,
     CASE
         WHEN customer_key LIKE 'NAS%' THEN SUBSTRING(customer_key, 4, LEN(customer_key)) 
-        -- Remove 'NAS' prefix in customer_key if present
         ELSE customer_key
-    END AS customer_key_cleaned,
-    birthdate,
+    END AS customer_key,
+    -- Remove 'NAS' prefix in customer_key if present
     CASE
         WHEN birthdate > GETDATE() THEN NULL
         ELSE birthdate
-    END AS birthdate_cleansed, 
+    END AS birthdate, 
     -- Set future birthdates to NULL
-    gender,
     CASE
         WHEN UPPER(TRIM(gender)) IN ('F', 'FEMALE') THEN 'Female'
         WHEN UPPER(TRIM(gender)) IN ('M', 'MALE') THEN 'Male'
         ELSE 'n/a'
-    END AS gender_cleansed
+    END AS gender
     -- Normalize gender values and handle unknown cases
 FROM 
     erp_customers_cleansed
