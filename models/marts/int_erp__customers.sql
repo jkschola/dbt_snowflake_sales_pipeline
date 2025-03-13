126ed2d6e9fb4e080d86f595d750316cecf5909c
Schola Janvier <90581570+jkschola@users.noreply.github.com>
2025-03-13 02:14:44 +0100

    Remove 'NAS' prefix in customer_key if present

--- models/marts/int_erp__customers.sql
+++ models/marts/int_erp__customers.sql
@@ -5,6 +5,11 @@ WITH erp_customers AS (
 )
 SELECT
     customer_key,
+    CASE
+        WHEN customer_key LIKE 'NAS%' THEN SUBSTRING(customer_key, 4, LEN(customer_key)) 
+        -- Remove 'NAS' prefix in customer_key if present
+        ELSE customer_key
+    END AS customer_key_cleaned,
     birthdate,
     gender
 FROM 
