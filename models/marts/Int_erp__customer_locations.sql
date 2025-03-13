f88b534c56d0081f1587121bed4ea78531803e3d
Schola Janvier <90581570+jkschola@users.noreply.github.com>
2025-03-13 02:05:23 +0100

    Standardize customer_key and Normalize and Handle missing or blank country codes

--- models/marts/Int_erp__customer_locations.sql
+++ models/marts/Int_erp__customer_locations.sql
@@ -4,7 +4,13 @@ WITH erp_customer_locations AS (
         {{ ref('stg_erp__customer_locations') }}
 )
 SELECT
-    customer_key,
-    country
+    REPLACE( customer_key, '-', '') AS customer_key,
+    CASE 
+        WHEN TRIM(country) = 'DE' THEN 'Germany'
+        WHEN TRIM(country) IN ( 'US', 'USA' ) THEN 'United States'
+        WHEN TRIM(country) = '' OR country IS NULL THEN 'n/a'
+        ELSE TRIM(country)
+    END AS country
+    -- Normalize and Handle missing or blank country codes
 FROM 
     erp_customer_locations
